540f3add8947fb128a5583c4c021e975e446435e
Zengxian Ding <zengxian.thomas@gmail.com>
2021-09-18 16:22:17 +0800

    fuzz: add sample code for tls

---
+++ tls/tls_test.go
@@ -0,0 +1,56 @@
+package tls_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/s3nt3/gofuzz-sample/tls"
+)
+
+func read(filename string) ([]byte, error) {
+	fd, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+
+	out, err := ioutil.ReadAll(fd)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func corpus(dir string, f *testing.F) {
+	d, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return
+	}
+
+	for _, seed := range d {
+		if seed.IsDir() {
+			corpus(path.Join(dir, seed.Name()), f)
+		} else {
+			s, err := read(path.Join(dir, seed.Name()))
+			if err != nil {
+				continue
+			}
+
+			f.Add(s)
+		}
+	}
+
+}
+
+func FuzzDES(f *testing.F) {
+	corpus("./corpus", f)
+
+	f.Fuzz(func(t *testing.T, payload []byte) {
+		t.Parallel()
+
+		tls.TLS(payload)
+	})
+}
